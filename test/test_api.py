419c2ea3df0c1555c2f5cd2eb41e1c2c47329493
Elena Tomlinson <elenaisabeltomlinson@gmail.com>
2022-05-23 10:15:04 -0700

    api token

--- test/test_api.py
+++ test/test_api.py
@@ -17,7 +17,7 @@ __license__ = "MIT"
 URL = "https://www.notion.so/Quick-Note-8a7ddc57165549a88da4c073c214ffb9"
 
 def test():
-    importer = NotionFileImporterPlugin(config={"apikey": "secret_myYSko8yPt5O8CwJgInPUthUD6XSsWwzKcKmcvXwjrR"})
+    importer = NotionFileImporterPlugin(config={"apikey": "<secret>"})
     request = PluginRequest(data=FileImportPluginInput(url=URL))
     response = importer.run(request)
     # print(response)
